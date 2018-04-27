83faf7a4080691ca0fbb5e35db61e467695eaccc
Keenan Baynard <ksbaynard@hotmail.com>
2018-04-27 16:45:37 -0400

    add entity relationship framework

---
+++ RReviews.DAL/ModelConvert.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using RestaurantModels;
+
+namespace RReviews.DAL
+{
+    public static class ModelConvert
+    {
+        public static DataSet LibraryToData(RestaurantModels.Restaurant model)
+        {
+            return null;
+        }
+        public static DataSet LibraryToData(RestaurantModels.Review model)
+        {
+            return null;
+        }
+        public static RestaurantModels.Restaurant DataToLibraryRestaurant(DataSet dataSet)
+        {
+            return null;
+        }
+        public static RestaurantModels.Review DataToLibraryReview(DataSet dataset)
+        {
+            return null;
+        }
+    }
+}
---
+++ RReviews.DAL/SearchRestaurant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RReviews.DAL
+{
+    class SearchRestaurant
+    {
+        RReviewsEntities db = new RReviewsEntities();
+
+        public IEnumerable<Restaurant> ShowRestaurants()
+        {
+            return null;
+        }
+    }
+}
